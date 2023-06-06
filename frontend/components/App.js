adb7866b1f6208e280f8386785d0df3db05a6e0b
petertrai <petertrai@gmail.com>
2023-06-05 19:52:39 -0700

    add input value tracker

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,32 +2,39 @@ import React from 'react'
 import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
-
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: []
+      todos: [],
+      error: '',
+      todoNameInput: ''
     }
   }
   fetchAllTodos = () => {
     axios.get(URL)
       .then(res => {
+        console.log(res);
+
         this.setState({ ...this.state, todos: res.data.data })
       })
       .catch(err => {
-        debugger
+        this.setState({ ...this.state, error: err.response.data.message })
       })
+  }
+  onTodoNameInputChange = (e) => {
+    const { value } = e.target
+    this.setState({ ...this.state, todoNameInput: value })
   }
 
   componentDidMount() {
-this.fetchAllTodos()
+    this.fetchAllTodos();
   }
 
   render() {
     return (
       <div>
-        <div id='error'>Error: No error here</div>
+        <div id='error'>{this.state.error}</div>
         <div id='todos'>
           <h2>Todos:</h2>
           {this.state.todos.map(td => {
@@ -35,7 +42,7 @@ this.fetchAllTodos()
           })}
         </div>
         <form id='todoForm'></form>
-        <input type='text' placeholder='Type todo'></input>
+        <input onChange={this.onTodoNameInputChange} value={this.state.todoNameInput} type='text' placeholder='Type todo'></input>
         <input type='submit'></input>
         <button>Clear Completed</button>
       </div>
